3696a0469e0dff6ebf93c787ab69aebf780e42ba
vikram0888 <vikramjha753@gmail.com>
2023-07-27 15:22:48 +0530

    requirements& readme

--- setup.py
+++ setup.py
@@ -6,13 +6,16 @@ VERSION = "0.0.1"
 AUTHOR = "vikram jha"
 AUTHOR_EMAIL_ID = "jhav7662@gmail.com"
 REQUIREMENTS_FILE_NAME = 'requirements.txt'
+HYPHEN_E_DOT="-e ."
 
-
-def get_requirements(file_path= REQUIREMENTS_FILE_NAME)->list(str):
+def get_requirements(file_path:str= REQUIREMENTS_FILE_NAME)->List[str]:
     requirements =[]
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        
+        #remove '/n' which represent new line internally
+        requirements=[req.replace("\n", "")for req in requirements]
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)    
     return requirements
 
 setup(name=NAME,
